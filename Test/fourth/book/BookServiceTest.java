cdf71d0cd9bd10e3c1f64053da3ee1c762d88b1b
Chirak <Pyc2002@yandex.ru>
2023-09-21 15:38:05 +0300

    HomeWork_4

--- Test/fourth/book/BookServiceTest.java
+++ Test/fourth/book/BookServiceTest.java
@@ -1,11 +1,12 @@
 package fourth.book;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 
-import static org.assertj.core.api.FactoryBasedNavigableListAssert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class BookServiceTest {
@@ -29,5 +30,24 @@ class BookServiceTest {
         bookService.findBookById("2");
 
         verify(bookRepository, times(1)).findById("2");
+    }
+    @Test
+    void findByIdTest(){
+        when(bookRepository.findById("0")).thenReturn(new Book("0", "Название", "Автор"));
+
+        assertEquals(bookRepository.findById("0"), bookService.findBookById("0"));
+    }
+
+    @Test
+    void findAllTest(){
+        List<Book> list = new ArrayList<>();
+        Book bookOne = new Book("0", "Назание", "Автор");
+        Book bookTwo = new Book("1", "Название1", "Автор2");
+        list.add(bookOne);
+        list.add(bookTwo);
+
+        when(bookRepository.findAll()).thenReturn(list);
+
+        assertEquals(bookRepository.findAll(), bookService.findAllBooks());
     }
 }
